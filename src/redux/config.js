a42d98fe91214c6ed5a472ec06bdfad837de9c38
issach315 <issachemani227@gmail.com>
2025-04-02 10:48:54 +0530

    changed the API BASE URL

--- src/redux/config.js
+++ src/redux/config.js
@@ -4,7 +4,7 @@ const apiConfig = require('./apiConfig');
 
 // const BASE_URL = apiConfig.PROD_appconfig.PROD_BASE_URL;
 
-const BASE_URL = 'http://192.168.0.135:8080';
+const BASE_URL = 'http://182.18.177.16:444';
 
 
 export default BASE_URL
