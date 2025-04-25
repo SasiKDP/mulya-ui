fbb3829102f579eaeff03907b54a93cd1b33e268
Vivek <work.vivek13@gmail.com>
2025-04-25 12:00:47 +0530

    Added Self requirements and client requirements for BDM

--- src/redux/requirementSlice.js
+++ src/redux/requirementSlice.js
@@ -1,6 +1,37 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import httpService from "../Services/httpService";
 
+
+export const fetchAllRequirementsBDM = createAsyncThunk(
+  'requirements/All/BDM',
+  async(_, {rejectWithValue}) => {
+      try{
+        const response = await httpService.get('/requirements/getAssignments');
+  
+        
+        return response.data;
+      }catch(error){
+        console.log(error);
+        return rejectWithValue(error);
+      }
+     }
+)
+
+export const fetchRequirementsBdmSelf = createAsyncThunk(
+  'requirements/BDM/self',
+  async(_, {getState,rejectWithValue}) => {
+      try{
+        const state = getState();
+        const userId = state.auth.userId;
+        const response = await httpService.get(`/requirements/bdmrequirements/${userId}`);
+        return response.data || [];
+      }catch(error){
+        console.log(error);
+        return rejectWithValue(error);
+      }
+     }
+)
+
 export const filterRequirementsByDateRange = createAsyncThunk(
     'requirements/filterByDateRange',
     async({startDate, endDate}, {rejectWithValue}) => {
@@ -47,37 +78,15 @@ export const getRequirementDetailsByJobId = createAsyncThunk(
   }
 );
 
-// export const getRequirementDetailsByJobId = createAsyncThunk(
-//   'requirements/getByJobId',
-//   async (jobId, { rejectWithValue }) => {
-//     try {
-//       // Use the fetch API to make the request
-//       const response = await fetch(`http://192.168.0.227:8111/requirements/${jobId}`);
-
-//       // Check if the response is not okay (status code not in the range 200-299)
-//       if (!response.ok) {
-//         throw new Error('Failed to fetch data');
-//       }
-
-//       // Parse the response body as JSON
-//       const data = await response.json();
-//       return data;
-//     } catch (error) {
-//       console.error('Error fetching requirement by job ID:', error);
-//       return rejectWithValue(error.message);  // Return the error message
-//     }
-//   }
-// );
-
-
-
 const requirementSlice =  createSlice({
     name: "requirement",
     initialState: {
         loading: false,
         filteredRequirementList: [],
         filterAssignedRequirements: [],
-        requirementDetails: null,
+        filteredTeamLeadRequirements: [],
+        requirementsAllBDM : [],
+        requirementsSelfBDM : [],
         isFilteredReqRequested: false,
         error: null
     },
@@ -88,6 +97,34 @@ const requirementSlice =  createSlice({
 
     }, extraReducers: (builder) => {
         builder
+
+        .addCase(fetchAllRequirementsBDM.pending, (state) => {
+          state.loading = true;
+          state.error = null;
+        })
+        .addCase(fetchAllRequirementsBDM.fulfilled, (state, action) => {
+          state.loading = false;
+          state.requirementsAllBDM = action.payload;
+        })
+        .addCase(fetchAllRequirementsBDM.rejected, (state, action) => {
+          state.loading = false;
+          state.error = action.payload;
+          
+        })
+
+        .addCase(fetchRequirementsBdmSelf.pending, (state) => {
+          state.loading = true;
+          state.error = null;
+        })
+        .addCase(fetchRequirementsBdmSelf.fulfilled, (state, action) => {
+          state.loading = false;
+          state.requirementsSelfBDM = action.payload;
+        })
+        .addCase(fetchRequirementsBdmSelf.rejected, (state, action) => {
+          state.loading = false;
+          state.error = action.payload;
+          
+        })
           // Filter Requirement List By date Range
           .addCase(filterRequirementsByDateRange.pending, (state) => {
             state.loading = true;
